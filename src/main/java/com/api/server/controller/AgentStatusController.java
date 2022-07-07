359d394f49d795046604fd59294af9bd5963049e
DESKTOP-FIF88VS\gos10 <n_nv711@naver.com>
2022-07-07 19:20:02 +0900

    feat:

--- src/main/java/com/api/server/controller/AgentStatusController.java
+++ src/main/java/com/api/server/controller/AgentStatusController.java
@@ -2,13 +2,10 @@ package com.api.server.controller;
 
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -16,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.api.server.model.agentstatus.AgentStatusCategoryResponse;
 import com.api.server.model.agentstatus.SearchAgentStatusCategory;
-import com.api.server.model.bookmark.CreateBookmark;
 import com.api.server.service.AgentStatusService;
 
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -43,13 +40,30 @@ public class AgentStatusController {
 		
         return agentStatusService.searchAgentStatusCategorys(searchAgentStatusCategory);
     }
-    
+	
 	
 	@ApiOperation("추가")
     @PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-    public void addBookmark(@Valid @RequestBody CreateBookmark createBookmark) throws Exception {
-    	//bookMarkService.createBookmark(createBookmark);
+    public void addBookmark() throws Exception {
     }
     
+	
+	@ApiOperation("레디스 상담유형")
+    @GetMapping("/redids/categorys")
+    public void getAgentCategory() throws Exception {
+    }
+	
+	
+	@ApiOperation("레디스 핵심문장 추출")
+	@GetMapping("/redids/sentences")
+    public void getAgentSentence() throws Exception {
+    }
+	
+	
+	@ApiOperation("레디스 상담정보")
+	@GetMapping("/redids/informations")
+    public void getAgentInformation() throws Exception {
+    }
+	
 }
