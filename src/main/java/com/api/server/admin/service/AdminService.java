8a8a8759375ede76ce248802d7a78ae741978c75
DESKTOP-FIF88VS\gos10 <n_nv711@naver.com>
2022-09-13 18:26:54 +0900

    feat: cache

--- src/main/java/com/api/server/admin/service/AdminService.java
+++ src/main/java/com/api/server/admin/service/AdminService.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,8 +13,11 @@ import com.api.server.admin.dao.AdminMapper;
 import com.api.server.admin.model.admin.AdminDeptResponse;
 import com.api.server.util.ResultMap;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
 @Transactional
+@Slf4j
 public class AdminService {
   
 	@Autowired
@@ -23,23 +28,36 @@ public class AdminService {
 		return adminRecentMap;
 	}
 	
+	
 	public List<ResultMap> selectMngAdvList(HashMap<String, Object> paramMap) {
 		return mapper.selectMngAdvList(paramMap);
 	}
+	
 	
 	public int insertInterest(HashMap<String, Object> paramMap) {
 		return mapper.insertInterest(paramMap);
 	}
 	
+	
 	public int deleteInterest(HashMap<String, Object> paramMap) {
 		return mapper.deleteInterest(paramMap);
 	}
+	
 	
 	public ResultMap selectMngAdvInterest(HashMap<String, Object> paramMap) {
 		return mapper.selectMngAdvInterest(paramMap);
 	}
 
+	
+	@Cacheable(cacheNames = "deptList", condition = "#companyCode != null")
 	public List<AdminDeptResponse> selectAdminDept(String companyCode) {
-		return  mapper.selectAdminDept(companyCode);
+		log.debug("$$ selectAdminDept");
+		return mapper.selectAdminDept(companyCode);
+	}
+
+	
+	@CacheEvict(cacheNames = "deptList", allEntries=true)
+	public void deleteAdminDeptCache() throws Exception {
+		log.debug("$$ deleteSelectAdminDeptCache");
 	}
 }
---
+++ src/main/java/com/api/server/config/CacheConfig.java
@@ -0,0 +1,25 @@
+package com.api.server.config;
+
+import java.util.Arrays;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+	@Bean
+	public CacheManager cacheManager() {
+		SimpleCacheManager simpleCacheManager = new SimpleCacheManager();
+		simpleCacheManager.setCaches(Arrays.asList(
+				new ConcurrentMapCache("holidayList"), 
+				new ConcurrentMapCache("deptList")));
+		return simpleCacheManager;
+	}
+	
+	
+}
